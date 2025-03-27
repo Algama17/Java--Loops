c2f0ecfa33b7ae6604317547c85ead1aaefe5dbf
Algama17 <albertogarciamallada@gmail.com>
2025-03-27 11:13:21 +0100

    second code

--- src/main/java/alberto/java_loops/MultiplicationTable.java
+++ src/main/java/alberto/java_loops/MultiplicationTable.java
@@ -2,6 +2,9 @@ package alberto.java_loops;
 
 public class MultiplicationTable {
     public String[] generateTable(int n) {
+        if (n < 0) {
+            throw new IllegalArgumentException("El valor de n no puede ser negativo");
+        }
         String[] table = new String[10];
         for (int i = 1; i <= 10; i++){
             int result = n * i;
